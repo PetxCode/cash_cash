ef993f5ee2c0243ab71e41ee8eb9feed39699795
PetxCode <brighterdayscodelab@gmail.com>
2024-05-30 12:33:59 +0100

    started here now

--- dist/utils/dbConfig.js
+++ dist/utils/dbConfig.js
@@ -11,7 +11,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.dbConfig = void 0;
 const mongoose_1 = require("mongoose");
-const url = "mongodb://127.0.0.1:27017/cashDB";
+// const url: string = "mongodb://127.0.0.1:27017/cashDB";
+const url = "mongodb+srv://brighterdayscodelab:brighterdayscodelab@cluster0.dmr8kfs.mongodb.net/AJCash?retryWrites=true&w=majority";
 const dbConfig = () => __awaiter(void 0, void 0, void 0, function* () {
     yield (0, mongoose_1.connect)(url)
         .then(() => {
--- utils/dbConfig.ts
+++ utils/dbConfig.ts
@@ -1,6 +1,9 @@
 import { connect } from "mongoose";
 
-const url: string = "mongodb://127.0.0.1:27017/cashDB";
+// const url: string = "mongodb://127.0.0.1:27017/cashDB";
+
+const url: string =
+  "mongodb+srv://brighterdayscodelab:brighterdayscodelab@cluster0.dmr8kfs.mongodb.net/AJCash?retryWrites=true&w=majority";
 
 export const dbConfig = async () => {
   await connect(url)
